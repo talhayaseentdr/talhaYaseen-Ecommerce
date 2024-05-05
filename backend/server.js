1eab9a084a8eced5ef53c728d274a053aa6d4b80
Talha Yaseen <talhayaseentdr@gmail.com>
2024-05-05 20:08:26 +0500

    first

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,25 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors')
 const app = express();
+const path = require("path")  //for buid project
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, './frontend/build')))
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next();
 })
 
+
 // routes 
 app.use('/api/user',authroutes);
 app.use('/api/category',categoryRoutes)
 app.use('/api/product',productRoutes)
+
+app.use("*", function(req,res) {
+    res.sendFile(path.join(__dirname,  "./frontend/build/index.html"))
+})
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> {
